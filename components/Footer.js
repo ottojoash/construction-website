2816a88f8450d676b392444fb78bc4cff30061cc
ottojoash <34677630+ottojoash@users.noreply.github.com>
2023-10-27 09:06:02 +0300

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,30 @@
 import { Container } from "react-bootstrap";
+import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer>
       <Container>
-        <br></br>
-        ©Oscar Truth
-        <br></br>
-        <br></br>
-        <br></br>
+        <br /><br />
+        <div>
+          © Oscar Truth
+          <br /><br />
+          <div>
+            {/* Social Media Links */}
+            <a href="https://twitter.com/your-twitter-profile">
+              <FaTwitter />
+            </a>
+            <a href="https://www.facebook.com/your-facebook-profile">
+              <FaFacebook />
+            </a>
+            <a href="https://www.instagram.com/your-instagram-profile">
+              <FaInstagram />
+            </a>
+            <a href="https://www.linkedin.com/in/your-linkedin-profile">
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
       </Container>
     </footer>
   );
