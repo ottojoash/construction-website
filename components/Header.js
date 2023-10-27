4c11abd5b4c44e8feb75a01238304a0306e12727
ottojoash <34677630+ottojoash@users.noreply.github.com>
2023-10-27 09:09:44 +0300

    header

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,6 @@ import { CgProfile } from "react-icons/cg";
 import { RiToolsFill, RiTrophyLine } from "react-icons/ri";
 import { BsInfoCircle } from "react-icons/bs";
 import { useRouter } from "next/router";
-// import logo from "/public/logo.png";
-// import Image from "next/image";
 
 export default function Header() {
   const router = useRouter();
@@ -14,27 +12,26 @@ export default function Header() {
     <header>
       <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
         <Container fluid style={{ width: "50vw" }}>
-          {/* <Navbar.Brand href="/"> */}
-            {/* <Image alt="logo" src={logo} /> */}
-          {/* </Navbar.Brand> */}
+          {/* Replace the logo with the name */}
+          <Navbar.Brand href="/">Oscar Truth</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav ">
+          <Navbar.Collapse id="responsive-navbar-nav">
             <Nav
-              className=" justify-content-between "
+              className="justify-content-between"
               style={{ width: "100%" }}
               activeKey={location}
             >
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about" className="d-flex align-items-center ">
+              <Nav.Link href="/about" className="d-flex align-items-center">
                 <BsInfoCircle /> &nbsp; About
               </Nav.Link>
-              <Nav.Link href="/projects" className="d-flex align-items-center ">
+              <Nav.Link href="/projects" className="d-flex align-items-center">
                 <RiTrophyLine /> &nbsp; Projects
               </Nav.Link>
-              <Nav.Link href="/services" className="d-flex align-items-center ">
+              <Nav.Link href="/services" className="d-flex align-items-center">
                 <RiToolsFill /> &nbsp; Services
               </Nav.Link>
-              <Nav.Link href="/contact" className="d-flex align-items-center ">
+              <Nav.Link href="/contact" className="d-flex align-items-center">
                 <CgProfile /> &nbsp; Contact
               </Nav.Link>
             </Nav>
